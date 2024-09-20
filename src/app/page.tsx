752441e0fffe972568117cd9ccce73d6f7e4b29a
thevinodpatidar <pvinod813@gmail.com>
2024-09-20 11:58:52 +0530

    home page button for login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { redirect, useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
   return (
     <main className="flex h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">DSA Learning Platform</h1>
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold text-center">
+          DSA Learning Platform
+        </h1>
+        <Button className="mt-8" onClick={() => router.push("/login")}>
+          Get Started
+        </Button>
+      </div>
     </main>
   );
 }
